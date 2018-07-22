4a5811c33b1d1151690977f86851d82019017141
tshi206 <tshi206@aucklanduni.ac.nz>
2018-07-22 17:54:00 +1200

    add webpack starter

---
+++ react_webpack4_starter/src/component/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>It works</h1>
+            </div>
+        );
+    }
+
+}
