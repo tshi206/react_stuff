35cac677cd641b3ca28262f44e9b3a542a611fbd
tshi206 <tshi206@aucklanduni.ac.nz>
2018-11-20 04:58:24 +1300

    add comment

--- spacex-launch-stats/server.js
+++ spacex-launch-stats/server.js
@@ -20,6 +20,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"))
 });
 
+// Heroku dynamically assigns your app a port, so you can't set the port to a fixed number. Heroku adds the port to the env, so you can pull it from there. Switch the hardcoded port number to this. That way it'll still listen to port 5000 when you test locally (i.e., process.env.PORT is undefined), but it will also work on Heroku.
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`GraphQL Server started on port ${PORT}`));
