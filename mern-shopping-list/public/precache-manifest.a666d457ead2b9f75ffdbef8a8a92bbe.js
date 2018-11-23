8f8f01d4d7e986e460aaf26ae02073959b46f3f1
tshi206 <tshi206@aucklanduni.ac.nz>
2018-11-24 03:02:36 +1300

    add built files for mern shopping list

---
+++ mern-shopping-list/public/precache-manifest.a666d457ead2b9f75ffdbef8a8a92bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f8b890b215d8f0819acd0e4fadd4162",
+    "url": "/static/media/loader.3f8b890b.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00903cc0c34ee18e5aa4",
+    "url": "/static/js/main.00903cc0.chunk.js"
+  },
+  {
+    "revision": "50d075dc8e0e8b148a5c",
+    "url": "/static/js/1.50d075dc.chunk.js"
+  },
+  {
+    "revision": "00903cc0c34ee18e5aa4",
+    "url": "/static/css/main.6bd7ebc2.chunk.css"
+  },
+  {
+    "revision": "50d075dc8e0e8b148a5c",
+    "url": "/static/css/1.ffb3b5df.chunk.css"
+  },
+  {
+    "revision": "db463b517f91be2cbbd2dc5ad77214d5",
+    "url": "/index.html"
+  }
+];
