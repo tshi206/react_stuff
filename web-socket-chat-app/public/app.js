2099c3ac211a7b0dece973a51139a19d974c14cf
tshi206 <tshi206@aucklanduni.ac.nz>
2018-11-25 22:04:04 +1300

    add chat app with socket io

---
+++ web-socket-chat-app/public/app.js
@@ -0,0 +1,39 @@
+// Make socket connection
+const socket = io.connect("http://localhost:4000");
+
+// Query DOM
+const message = document.getElementById("message");
+const handle = document.getElementById("handle");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+
+// Emit events
+btn.addEventListener("click", () => {
+    socket.emit("chat", {
+        message: message.value,
+        handle: handle.value
+    })
+});
+
+message.addEventListener("keypress", () => {
+    socket.emit("typing", {
+        username: !handle.value ? `Unknown User at socket id ${socket.id}` : handle.value
+    })
+});
+
+// Listen for events
+socket.on("chat", data => {
+    output.innerHTML += "<p><strong>" +
+        data.handle +
+        ": </strong>" +
+        data.message +
+        "</p>";
+    feedback.innerHTML = "";
+});
+
+socket.on("typing", username => {
+    feedback.innerHTML = "<p><em>" +
+        username +
+        " is typing a message...</em></p>"
+});
---
+++ web-socket-chat-app/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+const fs = require('fs');
+const promisfy = require('util').promisify;
+const readFile = promisfy(fs.readFile);
+const writeFile = promisfy(fs.writeFile);
+// fs.readFile(someFile, 'utf8', function (err,data) {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     var result = data.replace(/string to be replaced/g, 'replacement');
+//
+//     fs.writeFile(someFile, result, 'utf8', function (err) {
+//         if (err) return console.log(err);
+//     });
+// });
+
+(async () => {
+    const app = express();
+
+    const PORT = process.env.PORT || 4000;
+
+    // see if in production
+    const hostname = process.env.PORT ? `<production-host>:${PORT}` : `http://localhost:${PORT}`;
+
+    const server = app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+    // Middleware and routes
+    app.get("/app.js", async (req, res) => {
+        // make this particular route having higher priority than static files since we need to pre-process the app.js before serving it
+        const appJS = path.join(__dirname, 'public', 'app.js');
+        const appJSTemplate = path.join(__dirname, 'templates', 'app.js');
+
+        const str = await readFile(appJSTemplate, 'utf8');
+        const parsedStr = str.replace(/{{hostname}}/g, hostname);
+        await writeFile(appJS, parsedStr, 'utf8');
+        res.sendFile(appJS)
+    });
+
+    app.use(express.static('public'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    });
+
+    // Socket setup
+    const io = socket(server);
+
+    // Each client will have its own socket upon successfully established socket connection. Those individual 'socket' objects will get passed in to this event listener's callback whenever a new socket connection is established between the client and the server.
+    io["on"]('connection', socket => {
+        console.log('socket connection established', 'with the server at:', socket.handshake.headers.host, '; socket id:', socket.id);
+        socket.on('chat', data => {
+            io["sockets"].emit('chat', data)
+        });
+        socket.on('typing', data => {
+            // emit to every other clients but not the sender who originates the message
+            socket["broadcast"].emit('typing', data["username"]); // flatten the payload and send a string back instead of sending back the original payload object; client should expect a string back upon listening to the 'typing' events on the socket.
+        })
+    });
+})();
